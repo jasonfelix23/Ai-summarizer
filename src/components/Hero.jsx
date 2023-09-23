3ea285bd784c516f6ae58c47d8715d450cd56f97
Jason Felix <61311125+jasonfelix23@users.noreply.github.com>
2023-09-23 14:48:14 -0600

    Working app

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         </nav>
         <h1 className='head_text'>
             Summarize Articles with <br className='max-md:hidden'/>
-            <span className='purple_gradient'>Open-AI GPT-4</span>
+            <span className='orange_gradient'>Open-AI GPT-4</span>
         </h1>
         <h2 className='desc'>
             Simplify your reading with summarize, an open-source article 
